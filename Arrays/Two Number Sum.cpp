bb225a68393748229ee02caf02f9ec7558d1c14b
Abhay Singh <abhay5349singh@gmail.com>
2021-10-13 13:34:56 +0530

    Add files via upload

---
+++ Arrays/Two Number Sum.cpp
@@ -0,0 +1,23 @@
+#include <vector>
+using namespace std;
+
+vector<int> twoNumberSum(vector<int> a, int target) {
+  	vector<int> v;
+	sort(a.begin(),a.end());
+	int n=a.size();
+	int i=0, j=n-1;
+	while(i<j){
+		int x=a[i]+a[j];
+		if(x<target){
+			i++;
+		}else if(x>target){
+			j--;
+		}else{
+			v.push_back(a[i]);
+			v.push_back(a[j]);
+			i++, j--;
+		}
+	}
+  return v;
+}
+
