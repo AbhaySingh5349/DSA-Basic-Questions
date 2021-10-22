43ce1815728ebcd4670c9480fca495bc8e78da71
Abhay Singh <abhay5349singh@gmail.com>
2021-10-22 12:36:34 +0530

    Add files via upload

---
+++ Binary Search Tree/Min Height Bst from Inorder array.cpp
@@ -0,0 +1,13 @@
+BST *create(vector<int> &a, int s, int e){
+	if(s>e) return NULL;
+	int mid=(s+e)/2;
+	BST *node = new BST(a[mid]);
+	node->left=create(a,s,mid-1);
+	node->right=create(a,mid+1,e);
+	return node;
+}
+
+BST *minHeightBst(vector<int> a) {
+  int n=a.size();
+  return create(a,0,n-1);
+}
