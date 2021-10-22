052b789f5b4bbc262ec7a13e5810b1d0bb60ab88
Abhay Singh <abhay5349singh@gmail.com>
2021-10-23 00:54:52 +0530

    Update Find Bridge Edges in graph.cpp

--- Graphs/Find Bridge Edges in graph.cpp
+++ Graphs/Find Bridge Edges in graph.cpp
@@ -1,6 +1,21 @@
 class Solution {
 public:
     
+    
+// if there exist a Bridge-edge, so graph is not 2-edge connected
+// Tree-edges(can be a Bridge-edge): connects new vertices in DFS
+// Back-edge: edge b/t current node<-->vertex which is our visited descendent with less arrival time (ancestor but not parent)
+// Back-edge tell us there is another way to reach current vertex from ancestral vertex
+// for each existing Tree-edge pair in DFS, we need to check:
+// --> if we can reach from parent to child taking some other path than current direct edge
+// --> if any of descendents of child can reach parent, we can say parent--child is not Bridge-edge 
+// (minimum arrival time of ancestor that current vertex can access)
+
+// Forward-edge: vertex which is our visited descendent with > arrival time
+
+// we will return min arrival time from descendent
+// for any vertex, if min arrival time of child > arrival time of parent, so this vertex forms Bridge-edge with child
+    
     void dfs(vector<int> graph[],int src,int parent,int &time,vector<int> &arrivalTime,vector<int> &minArrivalTime,vector<vector<int>> &ans){
         arrivalTime[src]=time;
         minArrivalTime[src]=time;
