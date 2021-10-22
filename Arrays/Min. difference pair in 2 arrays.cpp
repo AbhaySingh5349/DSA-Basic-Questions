9eebacae3d2dd7e0bf9fc48c2bda8c30c8fdcfb1
Abhay Singh <abhay5349singh@gmail.com>
2021-10-22 12:41:49 +0530

    Update Min. difference pair in 2 arrays.cpp

--- Arrays/Min. difference pair in 2 arrays.cpp
+++ Arrays/Min. difference pair in 2 arrays.cpp
@@ -1,6 +1,8 @@
 #include <vector>
 using namespace std;
 
+// Approach 1: O(n^2)
+
 vector<int> smallestDifference(vector<int> a, vector<int> b) {
   int n=a.size(), m=b.size();
 	int mind=INT_MAX, f,s;
@@ -15,6 +17,37 @@ vector<int> smallestDifference(vector<int> a, vector<int> b) {
 		}
 	}
 	vector<int> v;
+	v.push_back(f);
+	v.push_back(s);
+  return v;
+}
+
+// Approach: O(nlogn)
+
+vector<int> smallestDifference(vector<int> a, vector<int> b) {
+  int n=a.size(), m=b.size();
+	int mind=INT_MAX, f,s;
+	
+	sort(a.begin(),a.end());
+	sort(b.begin(),b.end());
+	int i=0, j=0;
+	while(i<n && j<m){
+		int d=abs(a[i]-b[j]);
+		if(d<mind){
+				mind=d;
+				f=a[i];
+				s=b[j];
+			}
+		if(a[i]<b[j]){
+			i++;
+		}else if(a[i]>b[j]){
+			j++;
+		}else{
+			break;
+		}
+	}
+	
+  vector<int> v;
 	v.push_back(f);
 	v.push_back(s);
   return v;
