c81ac278ce65b0b92e864e5b1a908a787034dab7
Abhay Singh <abhay5349singh@gmail.com>
2022-03-27 15:54:24 +0530

    Update Non-constructible change.cpp

--- Arrays/Non-constructible change.cpp
+++ Arrays/Non-constructible change.cpp
@@ -1,3 +1,10 @@
+// Question: minimum sum of money that cannot be created
+
+"coins": [5, 7, 1, 1, 2, 3, 22]
+op: 20
+	
+// Method 1:
+
 #include <vector>
 using namespace std;
 
@@ -22,4 +29,21 @@ int nonConstructibleChange(vector<int> a) {
 	for(int i=n,j=0;j<sum+1;j++) if(dp[i][j]==false) return j; 
 	
 	return sum+1;
+}
+
+// Method 2: if current value is > than 'running sum+1' therefor we cannot create that value
+
+#include <vector>
+using namespace std;
+
+int nonConstructibleChange(vector<int> a) { 
+  int n=a.size();
+	if(n==0) return 1;
+	int sum=0;
+	sort(a.begin(),a.end());
+	for(int i=0;i<n;i++){
+		if(a[i]>sum+1) return sum+1;
+		sum+=a[i];
+	}
+  return sum+1;
 }
