d6df6b00843dd66f64cebf2b723d7464ff99916f
Abhay Singh <abhay5349singh@gmail.com>
2022-03-27 16:45:02 +0530

    Update First duplicate value.cpp

--- Arrays/First duplicate value.cpp
+++ Arrays/First duplicate value.cpp
@@ -1,3 +1,9 @@
+// Question: 
+"array": [2, 1, 5, 3, 3, 2 , 4]
+op: 3
+
+// Approach 1:
+
 #include <vector>
 using namespace std;
 
@@ -13,4 +19,22 @@ int firstDuplicateValue(vector<int> a) {
 		}
 	}
 	return (idx==n+1 ? -1:num); 
+}
+
+// Approach 2:
+
+#include <vector>
+using namespace std;
+
+int firstDuplicateValue(vector<int> a) { 
+	int n=a.size();
+	for(int i=0;i<n;i++){
+		int idx=abs(a[i])-1;
+		if(a[idx]>0){
+			a[idx]*=-1;
+		}else{
+			return abs(a[i]);
+		}
+	}
+	return -1; 
 }
