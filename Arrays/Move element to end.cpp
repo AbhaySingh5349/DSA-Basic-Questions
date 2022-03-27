bcc198bd89af3ac618ca7b627a174ae490c2a46f
Abhay Singh <abhay5349singh@gmail.com>
2022-03-27 16:02:00 +0530

    Update Move element to end.cpp

--- Arrays/Move element to end.cpp
+++ Arrays/Move element to end.cpp
@@ -1,3 +1,9 @@
+// Question: move all occurances of given element to end to array
+
+"array": [2, 1, 2, 2, 2, 3, 4, 2],
+"toMove": 2
+op: [1,3,4,2,2,2,2,2]
+
 #include <bits/stdc++.h>
 using namespace std;
 
