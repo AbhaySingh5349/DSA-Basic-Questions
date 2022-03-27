48cc5e353eb5b695bcb9d6c2466134c469330540
Abhay Singh <abhay5349singh@gmail.com>
2022-03-27 16:24:05 +0530

    Update Product except self.cpp

--- Arrays/Product except self.cpp
+++ Arrays/Product except self.cpp
@@ -1,3 +1,8 @@
+"array": [5, 1, 4, 2]
+op: [8,40,10,20]
+
+// Approach 1:
+
 #include <vector>
 
 using namespace std;
@@ -15,5 +20,27 @@ vector<int> arrayOfProducts(vector<int> a) {
 		v[i]=p;
 	}
 	
+  return v;
+}
+
+// Approach 2:
+
+#include <vector>
+
+using namespace std;
+
+vector<int> arrayOfProducts(vector<int> a) {
+  int n=a.size();
+	
+	int left[n], right[n];
+	
+	left[0]=1;
+	for(int i=1;i<n;i++) left[i]=left[i-1]*a[i-1];
+	
+	right[n-1]=1;
+	for(int i=n-2;i>=0;i--) right[i]=right[i+1]*a[i+1];
+	
+	vector<int> v(n);
+	for(int i=0;i<n;i++) v[i]=left[i]*right[i];
   return v;
 }
