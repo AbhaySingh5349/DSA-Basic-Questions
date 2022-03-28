d93229702cac4a5a726a8157502272ba5425b900
Abhay Singh <abhay5349singh@gmail.com>
2022-03-28 15:54:08 +0530

    Update Subarray sort.cpp

--- Arrays/Subarray sort.cpp
+++ Arrays/Subarray sort.cpp
@@ -1,3 +1,10 @@
+// Question: find start and end index of smallest subarray which if sorted in-place, complete array is sorted in ascending order
+
+"array": [1, 2, 4, 7, 10, 11, 7, 12, 6, 7, 16, 18, 19]
+op: [3,9]
+
+// Approach 1:
+
 #include <vector>
 using namespace std;
 
@@ -15,6 +22,28 @@ vector<int> subarraySort(vector<int> a) {
 				r=i;
 			}
 		}
+	}
+  return {l,r};
+}
+
+// Approach 2:
+
+#include <vector>
+using namespace std;
+
+vector<int> subarraySort(vector<int> a) {
+  int n=a.size();
+	
+	int l=-1, curmin=a[n-1];
+	for(int i=n-2;i>=0;i--){
+		if(a[i]>curmin) l=i;
+		curmin = min(curmin,a[i]);
+	}
+	
+	int r=-1, curmax=a[0];
+	for(int i=1;i<n;i++){
+		if(a[i]<curmax) r=i;
+		curmax=max(curmax,a[i]);
 	}
   return {l,r};
 }
