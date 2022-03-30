7bd5b358f4c106167e141e8e2577a48b8dbfa984
Abhay Singh <abhay5349singh@gmail.com>
2022-03-30 11:12:14 +0530

    Add files via upload

---
+++ Graphs/Remove islands.cpp
@@ -0,0 +1,45 @@
+#include <vector>
+using namespace std;
+
+void dfs(vector<vector<int>> &grid, int n, int m, int i, int j){
+	if(i<0 || j<0 || i>=n || j>=m || grid[i][j]!=1) return;
+	
+	grid[i][j]=2;
+	dfs(grid,n,m,i,j-1);
+	dfs(grid,n,m,i-1,j);
+	dfs(grid,n,m,i,j+1);
+	dfs(grid,n,m,i+1,j);
+}
+
+vector<vector<int>> removeIslands(vector<vector<int>> grid) {
+  int n=grid.size(), m=grid[0].size();
+	
+	// top wall
+	for(int i=0,j=0;j<m;j++){
+		if(grid[i][j]==1) dfs(grid,n,m,i,j);
+	}
+	// right wall
+	for(int i=1,j=m-1;i<n;i++){
+		if(grid[i][j]==1) dfs(grid,n,m,i,j);
+	}
+	// bottom wall
+	for(int i=n-1,j=m-2;j>=0;j--){
+		if(grid[i][j]==1) dfs(grid,n,m,i,j);
+	}
+	// left wall
+	for(int i=n-2,j=0;i>=1;i--){
+		if(grid[i][j]==1) dfs(grid,n,m,i,j);
+	}
+	
+	for(int i=1;i<n-1;i++){
+		for(int j=1;j<m-1;j++){
+			if(grid[i][j]==1) grid[i][j]=0;
+		}
+	}
+	for(int i=0;i<n;i++){
+		for(int j=0;j<m;j++){
+			if(grid[i][j]==2) grid[i][j]=1;
+		}
+	}
+  return grid;
+}
