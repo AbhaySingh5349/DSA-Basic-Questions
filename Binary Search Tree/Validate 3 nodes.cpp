caabaa1b31f6b7950a4a058f86acc5a454391a64
Abhay Singh <abhay5349singh@gmail.com>
2022-03-30 09:53:31 +0530

    Update Validate 3 nodes.cpp

--- Binary Search Tree/Validate 3 nodes.cpp
+++ Binary Search Tree/Validate 3 nodes.cpp
@@ -1,3 +1,4 @@
+// Question: given 3 nodes, check if nodeOne is ancestor of nodeTwo, then nodeThree is descendent or vice versa.
 
 // Approach 1: time and space: O(h)
 
@@ -58,3 +59,52 @@ bool validateThreeNodes(BST *nodeOne, BST *nodeTwo, BST *nodeThree) {
 				  (check(nodeThree,nodeTwo) && check(nodeTwo,nodeOne)));
 }
 
+// Approach 3:
+
+using namespace std;
+
+class BST {
+public:
+  int value;
+  BST *left = nullptr;
+  BST *right = nullptr;
+
+  BST(int value) { this->value = value; }
+};
+
+// time: O(d) where d = distance(nodeOne,nodeThree)
+// checking if nodeTwo lies on path nodeOne <--> nodeTwo
+
+bool findDescendent(BST *ancestor, BST *descendent){
+	if(ancestor==NULL) return false;
+	if(ancestor->value > descendent->value){
+		if(findDescendent(ancestor->left,descendent)) return true;
+	}else if(ancestor->value < descendent->value){
+		if(findDescendent(ancestor->right,descendent)) return true;
+	}else{
+		return true;
+	}
+	return false;
+}
+
+bool check(BST *ancestor, BST *node, BST *descendent, bool &foundNode, bool &foundDescendent){
+	if(ancestor==NULL) return false;
+	if(ancestor->value==descendent->value) foundDescendent=true;
+	if(foundDescendent && foundNode==false) return false; // pruning 
+	
+	if(ancestor->value > node->value){
+		if(check(ancestor->left,node,descendent,foundNode,foundDescendent)) return true;
+	}else if(ancestor->value < node->value){
+		if(check(ancestor->right,node,descendent,foundNode,foundDescendent)) return true;
+	}else{
+		foundNode=true;
+		if(findDescendent(ancestor,descendent)) return true;
+	}
+	return false;
+}
+
+bool validateThreeNodes(BST *nodeOne, BST *nodeTwo, BST *nodeThree) {
+	bool foundNode=false, foundDescendent=false;
+  return ((check(nodeOne,nodeTwo,nodeThree,foundNode,foundDescendent)) || 
+					(check(nodeThree,nodeTwo,nodeOne,foundNode,foundDescendent)));
+}
