2e029dd5fc07f7f103b97bcb5741d84e34a5302f
Abhay Singh <abhay5349singh@gmail.com>
2022-03-30 17:53:36 +0530

    Update Check if tree is Height balanced or not.cpp

--- Binary Trees/Check if tree is Height balanced or not.cpp
+++ Binary Trees/Check if tree is Height balanced or not.cpp
@@ -1,3 +1,7 @@
+// Question: check | left - right subtree height | <= 1
+
+// Approach 1:
+
 using namespace std;
 
 // This is an input class. Do not edit.
@@ -24,4 +28,35 @@ bool heightBalancedBinaryTree(BinaryTree *root) {
 	if(abs(lh-rh)>1) return false;
 	
   return (heightBalancedBinaryTree(root->left) && heightBalancedBinaryTree(root->right));
+}
+
+// Approach 2:
+
+using namespace std;
+
+// This is an input class. Do not edit.
+class BinaryTree {
+public:
+  int value;
+  BinaryTree *left = nullptr;
+  BinaryTree *right = nullptr;
+
+  BinaryTree(int value) { this->value = value; }
+};
+
+int height(BinaryTree *root, bool &isBal){
+	if(root==NULL) return 0;
+	
+	int lh=height(root->left,isBal);
+	int rh=height(root->right,isBal);
+	
+	if(abs(lh-rh)>1) isBal=false;
+	
+	return max(lh,rh)+1;
+}
+
+bool heightBalancedBinaryTree(BinaryTree *root) {
+  bool isBal=true;
+	height(root,isBal);
+  return isBal;
 }
