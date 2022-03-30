196192ed39097ee70102281d55471ab5bc42d029
Abhay Singh <abhay5349singh@gmail.com>
2022-03-30 06:51:21 +0530

    Update Minimum rewards.cpp

--- Arrays/Minimum rewards.cpp
+++ Arrays/Minimum rewards.cpp
@@ -2,6 +2,8 @@
 "scores": [8, 4, 2, 1, 3, 6, 7, 9, 5]
 op: 25 // [4,3,2,1,2,3,4,5,1]
 
+// Approach 1:
+
 #include <vector>
 using namespace std;
 
@@ -18,6 +20,26 @@ int minRewards(vector<int> a) {
 				if(a[j]>=a[j+1]) rewards[j]=max(rewards[j],rewards[j+1]+1);
 			}
 		}
+	}
+	int ans=0;
+	for(int i=0;i<n;i++) ans+=rewards[i];
+  return ans;
+}
+
+// Approach 2:
+
+#include <vector>
+using namespace std;
+
+int minRewards(vector<int> a) {
+  int n=a.size();
+	int rewards[n];
+	rewards[0]=1;
+	for(int i=1;i<n;i++){
+		rewards[i] = (a[i]>a[i-1] ? rewards[i-1]+1:1); 
+	}
+	for(int i=n-2;i>=0;i--){
+		rewards[i] = ((a[i]>a[i+1] && rewards[i]<=rewards[i+1]) ? rewards[i+1]+1:rewards[i]);
 	}
 	int ans=0;
 	for(int i=0;i<n;i++) ans+=rewards[i];
