650bd94a07911dd2e6099264d73406b875e963a4
Abhay Singh <abhay5349singh@gmail.com>
2022-03-30 17:43:18 +0530

    Update Inorder successor.cpp

--- Binary Trees/Inorder successor.cpp
+++ Binary Trees/Inorder successor.cpp
@@ -1,3 +1,5 @@
+//Approach 1:
+
 using namespace std;
 
 // This is an input class. Do not edit.
@@ -27,4 +29,43 @@ BinaryTree *findSuccessor(BinaryTree *root, BinaryTree *node) {
 		if(cur==node && i<v.size()-1) return v[i+1];
 	}
   return NULL;
+}
+
+// Approach 2:
+
+using namespace std;
+
+// This is an input class. Do not edit.
+class BinaryTree {
+public:
+  int value;
+  BinaryTree *left = nullptr;
+  BinaryTree *right = nullptr;
+  BinaryTree *parent = nullptr;
+
+  BinaryTree(int value) { this->value = value; }
+};
+
+BinaryTree *suc;
+bool found;
+
+void inorder(BinaryTree *root, BinaryTree *node){
+	if(root==NULL) return;
+	
+	inorder(root->left,node);
+	
+	if(found==true){
+		suc=root;
+		found=false;
+	}
+	if(node==root) found=true;
+	
+	inorder(root->right,node);
+}
+
+BinaryTree *findSuccessor(BinaryTree *root, BinaryTree *node) {
+  suc=NULL;
+	found=false;
+	inorder(root,node);
+  return suc;
 }
