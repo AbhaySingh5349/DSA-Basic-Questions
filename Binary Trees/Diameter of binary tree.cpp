f98e485322ba55aa3533a55ff7dd76b14d172065
Abhay Singh <abhay5349singh@gmail.com>
2022-03-30 16:41:26 +0530

    Add files via upload

---
+++ Binary Trees/Diameter of binary tree.cpp
@@ -0,0 +1,39 @@
+using namespace std;
+
+// This is an input class. Do not edit.
+class BinaryTree {
+public:
+  int value;
+  BinaryTree *left;
+  BinaryTree *right;
+
+  BinaryTree(int value) {
+    this->value = value;
+    left = nullptr;
+    right = nullptr;
+  }
+};
+
+struct node{
+    int NTL; // node to leaf path for current node
+    int LTL; // maximum leaf to leaf distance 
+};
+    
+struct node solve(BinaryTree* root){
+    if(root==NULL) return {-1,-1};
+        
+    struct node leftNode=solve(root->left);
+    struct node rightNode=solve(root->right);
+        
+    int curNTL = max(leftNode.NTL,rightNode.NTL)+1;
+    int curLTL = max(leftNode.NTL+rightNode.NTL+2,max(leftNode.LTL,rightNode.LTL));
+        
+    struct node curNode = {curNTL,curLTL};
+    return curNode;
+}
+
+int binaryTreeDiameter(BinaryTree *tree) {
+  if(tree->left==NULL && tree->right==NULL) return 0;
+  return solve(tree).LTL;
+}
+
