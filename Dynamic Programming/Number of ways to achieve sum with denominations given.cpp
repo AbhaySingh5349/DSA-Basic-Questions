7a813c741788817778fccd1350a5a771f2e0f75e
Abhay Singh <abhay5349singh@gmail.com>
2022-04-01 12:54:26 +0530

    Update Number of ways to achieve sum with denominations given.cpp

--- Dynamic Programming/Number of ways to achieve sum with denominations given.cpp
+++ Dynamic Programming/Number of ways to achieve sum with denominations given.cpp
@@ -1,3 +1,5 @@
+// Approach 1:
+
 #include <vector>
 using namespace std;
 
@@ -19,3 +21,23 @@ int numberOfWaysToMakeChange(int target, vector<int> coins) {
 	return solve(coins,n,target,memo);
 }
 
+// Approach 2:
+
+#include <vector>
+using namespace std;
+
+int numberOfWaysToMakeChange(int target, vector<int> coins) {
+  int n=coins.size();
+	int dp[n+1][target+1];
+	
+	for(int i=0,j=0;i<n+1;i++) dp[i][j]=1;
+	for(int i=0,j=1;j<target+1;j++) dp[i][j]=0;
+	
+	for(int i=1;i<n+1;i++){
+		for(int j=1;j<target+1;j++){
+			dp[i][j]=dp[i-1][j];
+			if(j-coins[i-1]>=0) dp[i][j]+=dp[i][j-coins[i-1]];
+		}
+	}
+  return dp[n][target];
+}
