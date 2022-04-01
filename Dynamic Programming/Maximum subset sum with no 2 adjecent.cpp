263d88415cf7375b52f26ebe4132184c3586d27c
Abhay Singh <abhay5349singh@gmail.com>
2022-04-01 12:50:20 +0530

    Update Maximum subset sum with no 2 adjecent.cpp

--- Dynamic Programming/Maximum subset sum with no 2 adjecent.cpp
+++ Dynamic Programming/Maximum subset sum with no 2 adjecent.cpp
@@ -1,3 +1,5 @@
+// Approach 1:
+
 #include <vector>
 using namespace std;
 
@@ -12,4 +14,26 @@ int maxSubsetSumNoAdjacent(vector<int> a) {
 		dp[i]=max(dp[i-1],a[i]+dp[i-2]);
 	}
   return dp[n-1];
+}
+
+// Approach 2:
+
+#include <vector>
+using namespace std;
+
+int maxSubsetSumNoAdjacent(vector<int> a) {
+  int n=a.size();
+	if(n==0) return 0;
+	if(n==1) return a[0];
+	
+	int preprev=a[0];
+	int prev=max(a[0],a[1]);
+	int ans=max(prev,preprev);
+	for(int i=2;i<n;i++){
+		int cur=max(prev,a[i]+preprev);
+		preprev=prev;
+		prev=cur;
+		ans=cur;
+	}
+	return ans;
 }
