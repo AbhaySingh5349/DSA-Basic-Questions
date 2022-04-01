92fecc8144c9c7e84009ec5c088dd05cdc89ffa6
Abhay Singh <abhay5349singh@gmail.com>
2022-04-01 12:59:16 +0530

    Update Minimum number of coins to achieve sum with denominations given.cpp

--- Dynamic Programming/Minimum number of coins to achieve sum with denominations given.cpp
+++ Dynamic Programming/Minimum number of coins to achieve sum with denominations given.cpp
@@ -1,3 +1,5 @@
+// Approach 1:
+
 #include <vector>
 using namespace std;
 
@@ -22,3 +24,22 @@ int minNumberOfCoinsForChange(int target, vector<int> coins) {
 	return (ans==INT_MAX ? -1:ans);
 }
 
+// Approach 2:
+
+#include <vector>
+using namespace std;
+
+int minNumberOfCoinsForChange(int target, vector<int> coins) {
+  int n=coins.size();
+	int dp[n+1][target+1];
+	for(int i=0;i<n+1;i++) dp[i][0]=0;
+	for(int j=1;j<target+1;j++) dp[0][j]=target+1;
+	
+	for(int i=1;i<n+1;i++){
+		for(int j=1;j<target+1;j++){
+			dp[i][j]=dp[i-1][j];
+			if(j-coins[i-1]>=0) dp[i][j]=min(dp[i-1][j],1+dp[i][j-coins[i-1]]);
+		} 
+	}
+  return (dp[n][target]>target ? -1:dp[n][target]);
+}
