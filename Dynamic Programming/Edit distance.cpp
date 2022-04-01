97eba8e424e67b3930e07f05d32a3facfc733114
Abhay Singh <abhay5349singh@gmail.com>
2022-04-01 13:09:41 +0530

    Update Edit distance.cpp

--- Dynamic Programming/Edit distance.cpp
+++ Dynamic Programming/Edit distance.cpp
@@ -1,3 +1,5 @@
+// Approach 1:
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -22,4 +24,27 @@ int levenshteinDistance(string s1, string s2) {
   int n=s1.length(), m=s2.length();
 	vector<vector<int>> memo(n+1,vector<int> (m+1,-1));
   return solve(s1,s2,n,m,memo);
+}
+
+// Approach 2:
+
+using namespace std;
+
+int levenshteinDistance(string s1, string s2) {
+  int n=s1.length(), m=s2.length();
+	int dp[n+1][m+1];
+	
+	for(int i=0;i<n+1;i++) dp[i][0]=i;
+	for(int j=1;j<m+1;j++) dp[0][j]=j;
+	
+	for(int i=1;i<n+1;i++){
+		for(int j=1;j<m+1;j++){
+			if(s1[i-1]==s2[j-1]){
+				dp[i][j]=dp[i-1][j-1];
+			}else{
+				dp[i][j]=1+min(dp[i-1][j-1],min(dp[i-1][j],dp[i][j-1]));
+			}
+		}
+	}
+  return dp[n][m];
 }
